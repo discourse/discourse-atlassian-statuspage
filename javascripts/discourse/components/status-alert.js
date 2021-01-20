b21977f381d8c62d9ef3810cf165a89468a9fa93
Jordan Vidrine <jordan@jordanvidrine.com>
2021-01-20 14:49:07 -0600

    Init commit

---
+++ javascripts/discourse/components/status-alert.js
@@ -0,0 +1,35 @@
+import Component from "@ember/component";
+const API_ENDPOINT = settings.status_endpoint;
+
+export default Component.extend({
+  showStatus: null,
+  statusText: null,
+  statusHref: null,
+
+  init() {
+    this._super(...arguments);
+    this.set("statusHref", settings.status_more_info_link.replace(/http(s)*:\/\//g,""));
+
+    if (settings.failed_status_test) {
+      console.log('should show')
+      this.set("showStatus", true);
+      this.set("statusText", settings.failed_status_text);
+    } else {
+      try {
+        fetch(API_ENDPOINT)
+        .then(response => response.json())
+        .then(data => {
+          let { status } = data;
+          console.log(status)
+          if (status.description !== "All Systems Operational" ) {
+            this.set("statusText", settings.failed_status_text)
+            this.set("showStatus", true)
+          }
+        }); 
+      } catch (error) {
+        console.warn(error)
+      }
+    }
+  },
+
+})
