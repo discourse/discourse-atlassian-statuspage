ad3e56d66f5b7708373e0bf059cf2874cca561c9
Jordan Vidrine <jordan@jordanvidrine.com>
2021-04-12 13:44:35 -0500

    FEATURE: Add custom options for three different indicators.
    This commit adds further customizable options depending on the indicator of the status received from atlassian.

--- javascripts/discourse/components/status-alert.js
+++ javascripts/discourse/components/status-alert.js
@@ -5,29 +5,40 @@ export default Component.extend({
   showStatus: null,
   statusText: null,
   statusHref: null,
+  inidcator: null,
 
   init() {
     this._super(...arguments);
-    this.set("statusHref", settings.status_more_info_link.replace(/http(s)*:\/\//g,""));
+    this.set(
+      "statusHref",
+      settings.status_more_info_link.replace(/http(s)*:\/\//g, "")
+    );
 
     if (settings.failed_status_test) {
       this.set("showStatus", true);
-      this.set("statusMessage", settings.failed_status_message);
+      this.set("statusMessage", settings.test_status_message);
     } else {
       try {
         fetch(API_ENDPOINT)
-        .then(response => response.json())
-        .then(data => {
-          let { status } = data;
-          if (status.description !== "All Systems Operational" ) {
-            this.set("statusMessage", settings.failed_status_message)
-            this.set("showStatus", true)
-          }
-        }); 
+          .then(response => response.json())
+          .then(data => {
+            let { status } = data;
+            if (status.description !== "All Systems Operational") {
+              let statusMessage =
+                status.inidcator.toLowerCase() === "minor"
+                  ? settings.minor_status_message
+                  : status.indicator.toLowerCase() === "major"
+                  ? settings.major_status_message
+                  : settings.critical_status_message;
+
+              this.set("statusMessage", statusMessage);
+              this.set("indictor", status.inidcator.toLowerCase());
+              this.set("showStatus", true);
+            }
+          });
       } catch (error) {
-        console.warn(error)
+        console.warn(error);
       }
     }
-  },
-
-})
+  }
+});
